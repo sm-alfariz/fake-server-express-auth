9a6165ed1dda1edefc7ca0658b4447d2487e7082
Efendi Hariyadi <samman.alfariz@gmail.com>
2022-12-20 22:24:31 +0800

    add upload with multer

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ const {join} = require('path')
 var { expressjwt: expjwt } = require("express-jwt");
 const app = express();
 const priv_key = join(__dirname, "jwtRS256.key");
+const multer = require('multer');
+
+//
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'upload/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + file.originalname)
+  }
+})
+const upload = multer({ dest: 'upload/', storage: storage })
 
 // See the react auth blog in which cors is required for access
 app.use((req, res, next) => {
@@ -201,6 +214,22 @@ app.use(function (err, req, res, next) {
     else {
         next(err);
     }
+});
+
+//upload
+// app.post('/upload',  multer().none(), function(req, res){
+//    console.log(req.body);
+//    res.send("recieved your request!");
+// });
+
+app.post('/upload', upload.single("foto"), function (req, res, next) {
+    //console.log(req.file, req.body)
+    let resp = {
+        "status": "success",
+        "dtdc_trans_id": 264856,
+        "message": "Data berhasil disimpan"  
+    }
+    res.send(resp);
 });
 
 // Starting the app on PORT 3000
